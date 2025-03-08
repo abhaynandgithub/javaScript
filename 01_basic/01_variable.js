17b0b882b782e3b874b9c41e7daf64fef34c5f79
Abhaynand Singh <133901253+abhaynandgithub@users.noreply.github.com>
2025-03-08 16:04:50 +0000

    01_variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=12345
+let acconutEmail ="abc.gmail.com"
+var password ="45445"
+accountCity="jaipur"
+let accountstate;
+// note: if i decalared a variables and did not assign any value that variable then it is showing undefined
+console.log(accountId);
+
+/*
+prefer not to use var 
+beacause of issue in block scope and functional scope
+
+
+*/
+
+
+
+acconutEmail="ram@.com"
+password="12121212"
+accountCity="Gorakhpur"
+console.table([accountId,acconutEmail,password,accountCity,accountstate]);
