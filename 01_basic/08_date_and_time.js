93f31e8b7407879c206ca231a21156ccf70b1241
Abhaynand Singh <133901253+abhaynandgithub@users.noreply.github.com>
2025-03-18 18:28:49 +0000

    date

---
+++ 01_basic/08_date_and_time.js
@@ -0,0 +1,9 @@
+// date is obeject
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toDateString());
+console.log(typeof mydate);
+console.log(mydate.toLocaleDateString());
+
+
