6c565c1a6141d8b9bd667d43e8a4de748554d506
Abhaynand Singh <133901253+abhaynandgithub@users.noreply.github.com>
2025-04-12 21:26:11 +0000

    arrow function

---
+++ 03_basic/02_Scope.js
@@ -0,0 +1,69 @@
+// let a = 10;
+// var b=10;
+// const c = 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Abhaynand"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Abhaynand"
+    if (username === "Abhaynand") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
